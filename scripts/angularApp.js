da8a34ccc420b0bb6d2cb80b649809c28eeb2ecc
Benoit Lagae <benoit.lagae@itextpdf.com>
2015-11-05 02:14:17 +0100

    Improve list object with URLs

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -1,9 +1,12 @@
 var app = angular.module('comicvault', []);
 
 app.controller('Main', ['$scope',
-function($scope){
-  $scope.welcome = 'Welcome to the Comic Vault, your daily fix for webcomics.'
-  $scope.comicIntro = 'Your currently saved comics are:';
-  $scope.comics = ['XKCD', 'Questionable Content', 'Oglaf', 'A Softer World'];
-}
+	function($scope){
+	  $scope.welcome = 'Welcome to the Comic Vault, your daily fix for webcomics.'
+	  $scope.comicIntro = 'Your currently saved comics are:';
+	  $scope.comics = [{title: 'XKCD', url: 'http://xkcd.com'},
+		{title: 'Questionable Content', url: 'http://questionablecontent.net'},
+		{title: 'Oglaf', url: 'http://oglaf.com'},
+		{title: 'A Softer World', url: 'http://asofterworld.com'}];
+	}
 ]);
