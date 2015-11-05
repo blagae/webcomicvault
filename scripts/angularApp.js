eaf2331a99d31f01d0177aaa171fe7078f88b810
Benoit Lagae <benoit.lagae@itextpdf.com>
2015-11-05 02:22:27 +0100

    Add form functionality in HTML and Angular

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -1,12 +1,27 @@
 var app = angular.module('comicvault', []);
 
 app.controller('Main', ['$scope',
-	function($scope){
-	  $scope.welcome = 'Welcome to the Comic Vault, your daily fix for webcomics.'
-	  $scope.comicIntro = 'Your currently saved comics are:';
-	  $scope.comics = [{title: 'XKCD', url: 'http://xkcd.com'},
-		{title: 'Questionable Content', url: 'http://questionablecontent.net'},
-		{title: 'Oglaf', url: 'http://oglaf.com'},
-		{title: 'A Softer World', url: 'http://asofterworld.com'}];
-	}
+function($scope){
+  $scope.welcome = 'Welcome to the Comic Vault, your daily fix for webcomics.'
+  $scope.comicIntro = 'Your currently saved comics are:';
+  $scope.comics = [{title: 'XKCD', url: 'http://xkcd.com'},
+	{title: 'Questionable Content', url: 'http://questionablecontent.net'},
+	{title: 'Oglaf', url: 'http://oglaf.com'},
+	{title: 'A Softer World', url: 'http://asofterworld.com'}];
+	
+  $scope.addComic = function(){
+    $scope.comics.push({title: $scope.title, url: cleanUrl($scope.url)});
+    $scope.title = '';
+    $scope.url = '';
+  };
+}
 ]);
+// TODO: make safe
+function cleanUrl(url) {
+	if (url.startsWith('http')) {
+		return url;
+	}
+	else {
+		return 'http://'+url;
+	}
+}
