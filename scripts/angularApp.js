ac20dbb5b8d63a7c855a7c041eedb0502bb3a6d3
Benoit Lagae <benoit.lagae@itextpdf.com>
2015-11-05 02:09:42 +0100

    Add list object

--- scripts/angularApp.js
+++ scripts/angularApp.js
@@ -1,7 +1,9 @@
 var app = angular.module('comicvault', []);
 
-app.controller('Main', [
-'$scope',
+app.controller('Main', ['$scope',
 function($scope){
-  $scope.contents = 'Welcome to the Comic Vault, your daily fix for webcomics.';
-}]);
+  $scope.welcome = 'Welcome to the Comic Vault, your daily fix for webcomics.'
+  $scope.comicIntro = 'Your currently saved comics are:';
+  $scope.comics = ['XKCD', 'Questionable Content', 'Oglaf', 'A Softer World'];
+}
+]);
