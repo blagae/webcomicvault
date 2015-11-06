4cc571532ed4e46e26eedd169baf339328a6a008
Benoit Lagae <benoit.lagae@itextpdf.com>
2015-11-06 06:48:03 +0100

    Stash current changes to create Angular routes

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,13 +1,38 @@
-var app = angular.module('webcomicvault', []);
+var app = angular.module('webcomicvault', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'Main',
+	  resolve: {
+		postPromise: ['comics', function(comics){
+		  return comics.getAll();
+		}]
+	  }
+    });
+
+  //$urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('Comics', [
+'$scope',
+'$stateParams',
+'comics',
+function($scope, $stateParams, comics){
+	$scope.comic = comics.comic[$stateParams.id]; // TODO: might have to be plural ?
+}]);
 
 app.controller('Main', ['$scope',
 function($scope){
   $scope.welcome = 'Welcome to the Web Comic Vault, your daily fix for online comics.'
   $scope.comicIntro = 'Your currently saved comics are:';
-  $scope.comics = [{title: 'XKCD', url: 'http://xkcd.com'},
-	{title: 'Questionable Content', url: 'http://questionablecontent.net'},
-	{title: 'Oglaf', url: 'http://oglaf.com'},
-	{title: 'A Softer World', url: 'http://asofterworld.com'}];
+  $scope.comics = [];
 	
   $scope.addComic = function(){
     $scope.comics.push({title: $scope.title, url: cleanUrl($scope.url)});
@@ -16,6 +41,21 @@ function($scope){
   };
 }
 ]);
+app.factory('comics', ['$http', function($http){
+  var o = {
+  comics: []
+  };
+    o.getAll = function() {
+		return $http.get('/comics').success(function(data){
+      angular.copy(data, o.comics);
+    });
+  };
+
+  return o;
+}]);
+
+
+
 // TODO: make safe
 function cleanUrl(url) {
 	if (url.startsWith('http')) {
