359fe9347e40cfb39c02feee184562bb46518085
Benoit Lagae <benoit.lagae@itextpdf.com>
2015-11-16 22:47:00 +0100

    Add basic Gulp file for front-end resources

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minify = require('gulp-minify-css');
 
-gulp.task('default', gulp.parallel('styles', 'scripts'));
+gulp.task('styles', function() {
+	return gulp.src('private/styles/*.css')
+	.pipe(sass())
+	.pipe(minify())
+	.pipe(concat("style.css"))
+	.pipe(gulp.dest('public/styles'));
+});
 
 gulp.task('scripts', function() {
 	return gulp.src('private/scripts/**/*.js')
@@ -10,3 +18,10 @@ gulp.task('scripts', function() {
 	.pipe(concat("ang.min.js"))
 	.pipe(gulp.dest('public/scripts'));
 });
+
+gulp.task('images', function() {
+	return gulp.src('private/images/*.*')
+	.pipe(gulp.dest('public/images'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'scripts', 'images'));
