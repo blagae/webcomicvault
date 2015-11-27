27766a25e9843525369fa7ec6d0485562bf7490e
Benoit Lagae <benoit.lagae@itextpdf.com>
2015-11-27 19:14:52 +0100

    Overhaul models, with bidirectional post-save hook
    Also, change id to 8 digit numbers, and improve seeding data

--- models/Models.js
+++ models/Models.js
@@ -1,11 +1,17 @@
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 var crypto = require('crypto');
+var _ = require("underscore");
+
+var autoIncrement = require('mongoose-auto-increment');
+var connection = mongoose.createConnection('mongodb://localhost/comics');
+
+autoIncrement.initialize(connection);
 
 var CategorySchema = new mongoose.Schema({
     name: {type: String, unique: true},
     description: String,
-    comics: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comic'}]
+    comics: [{type: Number, ref: 'Comic'}]
 });
 
 mongoose.model('Category', CategorySchema);
@@ -16,10 +22,10 @@ var ComicSchema = new mongoose.Schema({
     url: String,
     urlpattern: String,
     current: mongoose.Schema.Types.Mixed,
-    users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-    categories: [{type: mongoose.Schema.Types.ObjectId, ref: 'Category'}],
+    users: [{type: Number, ref: 'User'}],
+    categories: [{type: Number, ref: 'Category'}],
     likes: {type: Number, default: 0},
-    strips: [{strip: {type: mongoose.Schema.Types.ObjectId, ref: 'Strip'}}]
+    strips: [{type: Number, ref: 'Strip'}]
 });
 
 mongoose.model('Comic', ComicSchema);
@@ -30,9 +36,9 @@ var UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, unique: true},
     hash: String,
     salt: String,
-    comics: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comic'}],
+    comics: [{type: Number, ref: 'Comic'}],
     settings: mongoose.Schema.Types.Mixed,
-    strips: [{ strip: {type: mongoose.Schema.Types.ObjectId, ref: 'Strip'}, liketype: [{type: String, enum: states, default: 'like'}]}]
+    strips: [{strip: {type: Number, ref: 'Strip'}, liketype: [{type: String, enum: states, default: 'like'}]}]
 });
 
 
@@ -42,7 +48,7 @@ var StripSchema = new mongoose.Schema({
     url: String,
     sequence: Number,
     alt: String,
-    comic: {type: mongoose.Schema.Types.ObjectId, ref: 'Comic'},
+    comic: {type: Number, ref: 'Comic'},
     likes: {type: Number, default: 0}
 });
 
@@ -79,15 +85,111 @@ ComicSchema.pre('save', function (next) {
     next();
 });
 
+CategorySchema.plugin(autoIncrement.plugin, {
+    model: 'Category',
+    startAt: 20000001});
+StripSchema.plugin(autoIncrement.plugin, {
+    model: 'Strip',
+    startAt: 40000001});
+UserSchema.plugin(autoIncrement.plugin, {
+    model: 'User',
+    startAt: 30000001});
+ComicSchema.plugin(autoIncrement.plugin, {
+    model: 'Comic',
+    startAt: 10000001});
+
 var User = mongoose.model('User');
+var Comic = mongoose.model('Comic');
+var Category = mongoose.model('Category');
+var Strip = mongoose.model('Strip');
+
+ComicSchema.pre('save', function (next) {
+    this.users = _.unique(this.users);
+    this.categories = _.unique(this.categories);
+    this.strips = _.unique(this.strips);
+    next();
+});
+UserSchema.pre('save', function (next) {
+    this.comics = _.unique(this.comics);
+    this.strips = _.unique(this.strips);
+    next();
+});
+
+CategorySchema.pre('save', function (next) {
+    this.comics = _.unique(this.comics);
+    next();
+});
+
 ComicSchema.post('save', function () {
     var id = this._id;
     this.users.forEach(function (userid) {
         User.findOne({'_id': userid}).exec(function (err, user) {
-            if (!err && user) {
+            if (!err && user && user.comics && !_.contains(user.comics, id)) {
                 user.comics.push({'_id': id});
                 user.save();
             }
         });
     });
+    this.categories.forEach(function (catid) {
+        Category.findOne({'_id': catid}).exec(function(err, cat) {
+            if (!err && cat && cat.comics && !_.contains(cat.comics, id)) {
+                cat.comics.push({'_id': id});
+                cat.save();
+            }
+        });
+    });
+    this.strips.forEach(function (stripid) {
+        Strip.findOne({'_id': stripid}).exec(function(err, strip) {
+            if (!err && strip && strip.comics && !_.contains(strip.comics, id)) {
+                strip.comics.push({'_id': id});
+                strip.save();
+            }
+        });
+    });
+});
+
+UserSchema.post('save', function () {
+    var inst = this._id;
+    this.comics.forEach(function (comicid) {
+        Comic.findOne({'_id': comicid}).exec(function (err, comic) {
+            if (!err && comic && comic.users && !_.contains(comic.users, inst)) {
+                comic.users.push({'_id': inst});
+                comic.save();
+            }
+        });
+    });
+    this.strips.forEach(function (stripid) {
+        Strip.findOne({'_id': stripid}).exec(function (err, strip) {
+            if (!err && strip && strip.users && !_.contains(strip.users, inst)) {
+                strip.users.push({'_id': inst});
+                strip.save();
+            }
+        });
+    });
+});
+
+
+CategorySchema.post('save', function () {
+    var inst = this._id;
+    this.comics.forEach(function (comicid) {
+        Comic.findOne({'_id': comicid}).exec(function (err, comic) {
+            if (!err && comic && comic.users && !_.contains(comic.users, inst)) {
+                comic.categories.push({'_id': inst});
+                comic.save();
+            }
+        });
+    });
+});
+
+StripSchema.post('save', function () {
+    var inst = this._id;
+    if (this.comic) {
+        var c = this.comic;
+        Comic.findOne({'_id': c}).exec(function (err, comic) {
+            if (!err && comic && comic.strips && !_.contains(comic.strips, inst)) {
+                comic.strips.push({'_id': inst});
+                comic.save();
+            }
+        });
+    }
 });
